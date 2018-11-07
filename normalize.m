113be8d8d5668cae2efa3e5425d4e36ae550ea77
Karthika <karthikaramineni@gmail.com>
2018-11-08 00:34:27 +0530

    Update normalize.m

--- normalize.m
+++ normalize.m
@@ -2,7 +2,7 @@ function [r_norm, g_norm, b_norm] = normalize(im)
     r = im(:,:,1);
     g = im(:,:,2);
     b = im(:,:,3);
-    r_norm = (r - min(r)) / (max(r) - min(r));
-    g_norm = (g - min(g)) / (max(g) - min(g));
-    b_norm = (b - min(b)) / (max(b) - min(b));
+    r_norm = (r - min(r)) ./ (max(r) - min(r));
+    g_norm = (g - min(g)) ./ (max(g) - min(g));
+    b_norm = (b - min(b)) ./ (max(b) - min(b));
 end
