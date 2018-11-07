210b2b4751672b75233878ef256e008a295c9647
16soumya <soumyataurani16@gmail.com>
2018-11-08 00:07:55 +0530

    adaptive masking

--- adaptivemasking.m
+++ adaptivemasking.m
@@ -1,4 +1,4 @@
-function [M1_r,M1_g,M1_b,M2_r,M2_g,M2_b] = normalize(im_r,im_g,im_b,t1_r,t1_g,t1_b,t2_r,t2_g,t2_b)
+function [M1_r,M1_g,M1_b,M2_r,M2_g,M2_b] = adaptivemasking(im_r,im_g,im_b,t1_r,t1_g,t1_b,t2_r,t2_g,t2_b)
     M1_r=im_r>t1_r;
     M1_g=im_g>t1_g;
     M1_b=im_b>t1_b;
